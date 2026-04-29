e86b8e522be19848c6491e6fd4c14e3dabd575ba
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2026-04-29 00:44:24 -0700

    Refactor main

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bytes"
 	"cmp"
+	"context"
 	"embed"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync/atomic"
 	"text/template"
+	"time"
 
 	"github.com/crhntr/channels/broadcast"
 	"github.com/typelate/sse"
@@ -21,13 +24,15 @@ var templateFiles embed.FS
 var templates = template.Must(template.ParseFS(templateFiles, "*"))
 
 type Server struct {
-	count   int64
-	updates chan<- int64
+	count            int64
+	countUpdates     chan int64
+	countCoordinator *broadcast.Coordinator[int64]
+	logger           *log.Logger
 }
 
 func (s *Server) Count() int64 {
 	n := atomic.AddInt64(&s.count, 1)
-	s.updates <- n
+	s.countUpdates <- n
 	return n
 }
 
@@ -35,40 +40,60 @@ func (s *Server) Index() int64 {
 	return atomic.LoadInt64(&s.count)
 }
 
-func main() {
+func (s *Server) Updates(res http.ResponseWriter, req *http.Request) {
+	c, closeSub := s.countCoordinator.SubscribeAll(1)
+	defer closeSub()
+	src, ok := sse.New(res, req, http.StatusOK)
+	if !ok {
+		s.logger.Println("new event source failed")
+		http.Error(res, "failed to create sse connection", http.StatusBadRequest)
+		return
+	}
+	buf := bytes.NewBuffer(nil)
+	for update := range c {
+		buf.Reset()
+		if err := templates.ExecuteTemplate(buf, "hello", update); err != nil {
+			s.logger.Println("execute template error", err)
+			continue
+		}
+		if err := src.Message(buf.Bytes()); err != nil {
+			s.logger.Println("sse send error", err)
+			continue
+		}
+	}
+}
+
+func (s *Server) routes() http.Handler {
 	mux := http.NewServeMux()
-	s := new(Server)
-	updates := make(chan int64, 5)
-	s.updates = updates
-	bc := broadcast.New(updates)
 	TemplateRoutes(mux, s)
+	mux.HandleFunc("GET /updates", s.Updates)
+	return mux
+}
 
-	mux.HandleFunc("GET /updates", func(res http.ResponseWriter, req *http.Request) {
-		c, closeSub := bc.SubscribeAll(5)
-		defer closeSub()
-		src, ok := sse.New(res, req, http.StatusOK)
-		if !ok {
-			log.Println("new event source failed")
-			http.Error(res, "failed to create sse connection", http.StatusBadRequest)
-			return
-		}
-		buf := bytes.NewBuffer(nil)
-		for update := range c {
-			buf.Reset()
-			if err := templates.ExecuteTemplate(buf, "hello", update); err != nil {
-				log.Println("execute template error", err)
-				continue
-			}
-			if err := src.Message(buf.Bytes()); err != nil {
-				log.Println("sse send error", err)
-				continue
-			}
-		}
-	})
+func addr() string { return cmp.Or(os.Getenv("HTTP_ADDR"), ":"+cmp.Or(os.Getenv("PORT"), "8080")) }
 
-	addr := cmp.Or(os.Getenv("HTTP_ADDR"), ":"+cmp.Or(os.Getenv("PORT"), "8080"))
-	log.Println("Starting http server", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
-		log.Fatalln(err)
+func main() {
+	c := make(chan int64, 1)
+	defer close(c)
+	s := &Server{
+		countUpdates:     c,
+		countCoordinator: broadcast.New(c),
+		logger:           log.Default(),
+	}
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+	srv := &http.Server{Addr: addr(), Handler: s.routes(), ErrorLog: s.logger}
+	s.logger.Println("starting http server", srv.Addr)
+	go func() {
+		<-ctx.Done()
+		s.logger.Println("shutting down http server", srv.Addr)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			s.logger.Println("server shutdown error", err)
+		}
+	}()
+	if err := srv.ListenAndServe(); err != nil {
+		s.logger.Fatalln(err)
 	}
 }
